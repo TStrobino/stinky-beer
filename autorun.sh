4e6de4fc70678c01cf2bd94b35a314bdf9ef09ce
jbwaring <jean-baptiste@zetane.com>
2022-05-23 14:21:28 -0400

    update autorun.sh

---
+++ autorun.sh
@@ -0,0 +1,8 @@
+pip3 install pipreqs
+
+# Run in current directory
+python3 -m  --force pipreqs.pipreqs .
+
+pip install -r requirements.txt
+
+python3 main.py
--- resolve_requirements.sh
+++
@@ -1,6 +0,0 @@
-pip3 install pipreqs
-
-# Run in current directory
-python3 -m  pipreqs.pipreqs .
-
-pip install -r requirements.txt
