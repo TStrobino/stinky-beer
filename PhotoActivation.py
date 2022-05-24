48c6d33e4fdced214877c42e4eec2414d716931a
Jean-Baptiste Waring <jean.baptiste.waring@icloud.com>
2022-05-23 21:57:34 -0400

    added postgres db

--- PhotoActivation.py
+++ PhotoActivation.py
@@ -1,4 +1,6 @@
 
+DEBUG = True
+
 ### GPIO PORT NUMBERS
 RED_PORT = 17
 GREEN_PORT = 27
@@ -7,20 +9,33 @@ BLUE_PORT = 22
 import sys
 import time
 import RPi.GPIO as GPIO
+import config
 def setup_GPIO():
+    GPIO.cleanup()
+    if ~DEBUG:
+        GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RED_PORT, GPIO.OUT)
     GPIO.setup(GREEN_PORT, GPIO.OUT)
     GPIO.setup(BLUE_PORT, GPIO.OUT)
 
+def stop_GPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
+
+
 def setRed(value):
-    GPIO.PWM(RED_PORT, value)
+    GPIO.PWM(RED_PORT, value/255)
+    if DEBUG:
+        print(f"[PhotoActivation setRed: {value}]")
 
 def setGreen(value):
-    GPIO.PWM(GREEN_PORT, value)
-
+    GPIO.PWM(GREEN_PORT, value/255)
+    if DEBUG:
+        print(f"[PhotoActivation setGreen: {value}]")
 def setBlue(value):
-    GPIO.PWM(BLUE_PORT, value)
-
+    GPIO.PWM(BLUE_PORT, value/255)
+    if DEBUG:
+        print(f"[PhotoActivation setBlue: {value}]")
 
 
--- main.py
+++ main.py
@@ -6,21 +6,31 @@ from rich.panel import Panel
 from rich.text import Text
 import PhotoActivation
 import time
+import signal
+import sys
+import config
+import subprocess
+
 def main():
+    subprocess.run(["clear"])
     print("Init...")
+    # SETUP
+    signal.signal(signal.SIGINT, handle_sigint)
+
     print(Panel(Text("ASTRO YEAST STINKY BEER", justify="center", style="bold green")))
     PhotoActivation.setup_GPIO()
     PhotoActivation.setBlue(255)
-    time.sleep(1)
     PhotoActivation.setGreen(255)
-    time.sleep(1)
     PhotoActivation.setRed(255)
-    time.sleep(20)
     print("Exiting...")
+    PhotoActivation.stop_GPIO()
 
 
 
-
+def handle_sigint(sig, frame):
+    print('Stopping...')
+    PhotoActivation.stop_GPIO()
+    sys.exit(0)
 
 
 if __name__ == "__main__":
